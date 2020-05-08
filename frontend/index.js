d6bca1e6a5fb4d9519ab9201f6782cd1d6fe18c5
CodeZeehun <codezeehun@gmail.com>
2020-05-08 21:23:15 +0800

    Slight HTML Indentation Fixed. Implemented Basic Data Viewer and Pagination Functionality

---
+++ frontend/index.js
@@ -0,0 +1,86 @@
+const basicDataQuery = {
+    companyId: null,
+    audienceCount: null,
+    page: 0,
+    pageSize: 5,
+};
+
+const basicDataPaginationFunction = {
+    gotoFirstPage: function() {
+        basicDataQuery['page'] = 0;
+    },
+    changePage: function(delta) {
+        console.log(delta);
+        basicDataQuery['page'] += parseInt(delta);
+    },
+    changePageSize: function(newPageSize) {
+        console.log(newPageSize);
+        basicDataQuery['pageSize'] = newPageSize;
+    }
+};
+
+const basicDataUrl = 'http://localhost:3000/basic/data';
+
+function populateBasicDataTable(data) {
+    console.log(data);
+    const dataTableHtml = data.map(
+        ({id, optionid, companyid, audiencecount, cost}) => `
+            <tr>
+                <th scope="row">${id}</th>
+                <td>${optionid}</td>
+                <td>${companyid}</td>
+                <td>${audiencecount}</td>
+                <td>${cost}</td>
+            </tr>
+    `,
+    );
+    $('#basic-data-tbody').html(dataTableHtml);
+}
+
+function getBasicDataFromBackEnd(callback) {
+    $.get(basicDataUrl, basicDataQuery)
+        .done((result) => callback(null, result))
+        .fail((message) => callback(message, null));
+}
+
+function refreshBasicDataTable() {
+    getBasicDataFromBackEnd(function(err, data) {
+        if (err) return alert(err);
+        populateBasicDataTable(data);
+    });
+}
+
+function filterBasicData(event) {
+    // This is for selecting those that are not input type submit.
+    // console.log($('#basic-data-filter-form input').not(':input[type=submit]')); 
+    $('#basic-data-filter-form input')
+        .not(':input[type=submit]')
+        .each((idx, input) => {
+            basicDataQuery[$(input).attr('key')] = $(input).val();
+        });
+    refreshBasicDataTable();
+    return false;
+}
+
+function registerBasicDataFilterForm() {
+    $('#basic-data-filter-form').submit(filterBasicData);
+}
+
+function paginateBasicData(event) {
+    const fn = $(this).attr('fn');
+    const value = $(this).attr('value') || $(this).val();
+    basicDataPaginationFunction[fn](value);
+    refreshBasicDataTable();
+}
+
+function registerBasicDataPaginationForm() {
+    $('#basic-data-first-page').click(paginateBasicData);
+    $('#basic-data-previous-page').click(paginateBasicData);
+    $('#basic-data-next-page').click(paginateBasicData);
+    $('basic-data-page-size-select').change(paginateBasicData);
+}
+
+$(document).ready(function() {
+    registerBasicDataFilterForm();
+    registerBasicDataPaginationForm();
+});
